ad8bf5c8d9dfd8cb22a940599f257768a1c332fd
WaelChb <144222573+WaelChb@users.noreply.github.com>
2024-05-19 17:21:23 +0200

    Portfolio end

---
+++ portfolio/src/components/CarouselImage.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="pic-ctn">
+    <img
+      v-for="(image, index) in images"
+      :key="index"
+      :src="image.src"
+      :alt="image.alt"
+      class="pic"
+      :width="image.width"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CarouselImage",
+  data() {
+    return {
+      images: [
+        {
+          src: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/1200px-HTML5_logo_and_wordmark.svg.png",
+          alt: "Html",
+          width: "150px",
+        },
+        {
+          src: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1200px-CSS3_logo_and_wordmark.svg.png",
+          alt: "Css",
+          width: "110px",
+        },
+        {
+          src: "https://logos-marques.com/wp-content/uploads/2021/03/JavaScript-Logo.png",
+          alt: "Js",
+          width: "280px",
+        },
+        {
+          src: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png",
+          alt: "Vue",
+          width: "170px",
+        },
+        {
+          src: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1200px-Node.js_logo.svg.png",
+          alt: "Node",
+          width: "150px",
+        },
+      ],
+    };
+  },
+};
+</script>
+<style>
+@keyframes display {
+  0% {
+    transform: translateX(200px);
+    opacity: 0;
+  }
+  10% {
+    transform: translateX(0);
+    opacity: 1;
+  }
+  20% {
+    transform: translateX(0);
+    opacity: 1;
+  }
+  30% {
+    transform: translateX(-200px);
+    opacity: 0;
+  }
+  100% {
+    transform: translateX(-200px);
+    opacity: 0;
+  }
+}
+
+.pic-ctn {
+  position: relative;
+  height: 300px;
+  margin-top: 15vh;
+}
+
+.pic-ctn > img {
+  position: absolute;
+  top: 0;
+  left: calc(50% - 100px);
+  opacity: 0;
+  animation: display 10s infinite;
+}
+
+img:nth-child(2) {
+  animation-delay: 2s;
+}
+img:nth-child(3) {
+  animation-delay: 4s;
+}
+img:nth-child(4) {
+  animation-delay: 6s;
+}
+img:nth-child(5) {
+  animation-delay: 8s;
+}
+</style>
--- portfolio/src/components/Competences.vue
+++ portfolio/src/components/Competences.vue
@@ -3,18 +3,68 @@
     <div class="titre2">
       <h3>Mes Compétences</h3>
     </div>
-    <div class="contenu4"></div>
+    <br />
+    <br />
+    <section>
+      <div class="contenu4">
+        <div
+          class="cardSkills"
+          v-for="(skills, category) in skillCategories"
+          :key="category"
+        >
+          <span class="titleSkills">{{ category }}</span>
+          <div class="card__tags">
+            <ul class="tag">
+              <li class="tag__name" v-for="skill in skills" :key="skill">
+                {{ skill }}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+    <CarouselImage />
   </div>
 </template>
+
 <script>
+import CarouselImage from "./CarouselImage.vue";
 export default {
   name: "CompetencesVue",
+  components: {
+    CarouselImage,
+  },
+  data() {
+    return {
+      skillCategories: {
+        "Front-End": [
+          "Html",
+          "Css",
+          "VueJs",
+          "ReactJS",
+          "JavaScript",
+          "Wordpress",
+        ],
+        "Back-End": [
+          "Nodejs",
+          "ExpressJs",
+          ".Net C#",
+          "Mysql",
+          "Php",
+          "Entity Framework",
+        ],
+      },
+    };
+  },
 };
 </script>
+
 <style>
 .container6 {
   margin-right: auto;
   margin-left: auto;
+  padding-right: 550px;
+  padding-left: 550px;
   width: 100%;
   display: flex;
   flex-direction: column;
@@ -24,9 +74,70 @@ export default {
 .titre2 {
   margin-top: 100px;
 }
+
 .contenu4 {
   display: flex;
   justify-content: space-between;
-  gap: 40px;
+  gap: 0px;
+}
+
+.cardSkills {
+  width: 34%;
+  height: 300px;
+  background: #171717;
+  padding: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  color: #31572c;
+  border-radius: 15px;
+}
+
+.card__tags {
+  overflow: auto;
+  height: 80%;
+}
+
+.titleSkills {
+  font-weight: 900;
+  font-size: 1.7em;
+}
+
+.tag__name {
+  display: inline-block;
+  color: #fff;
+  font-size: 1.1em;
+  background-color: black;
+  padding: 6px 23px 9px;
+  border-radius: 70em;
+  margin: 8px 6px 8px 0;
+  margin-left: 0px;
+  position: relative;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+}
+
+.tag__name::before,
+.tag__name::after {
+  content: "";
+  display: inline-block;
+  position: absolute;
+  top: 40%;
+  width: 7px;
+  height: 7px;
+  border-radius: 50%;
+  background: red;
+}
+
+.tag__name::before {
+  left: 7px;
+}
+
+.tag__name::after {
+  right: 7px;
+}
+
+.tag__name:hover {
+  transform: scale(1.1);
+  background-color: rgba(249, 154, 84);
 }
 </style>
