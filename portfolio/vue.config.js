e25c40839145fb12d4c1b895a8dd6acd59b2d9c2
WaelChb <144222573+WaelChb@users.noreply.github.com>
2024-05-23 23:54:10 +0200

    Update vue.config.js

--- portfolio/vue.config.js
+++ portfolio/vue.config.js
@@ -4,7 +4,7 @@ const webpack = require("webpack");
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    proxy: "https://portfolio-git-main-waelchbs-projects.vercel.app/",
+    proxy: "https://portfolio-two-chi-60.vercel.app/",
   },
   chainWebpack: (config) => {
     config.module
